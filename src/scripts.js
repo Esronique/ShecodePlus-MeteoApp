11cac537291d7eb77014de81429db984c0b80975
Esronique <rayneezdj@gmail.com>
2025-02-05 14:16:16 +0200

    Updating project look and feel

--- src/scripts.js
+++ src/scripts.js
@@ -4,30 +4,6 @@ function handleSearchSubmit(event) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
 }
-function changeTheme() {
-  let body = document.querySelector("body");
-  let headerElement = document.querySelector("header");
-  let footerElement = document.querySelector("footer");
-  let searchElement = document.querySelector("#search-form-input");
-  let searchButton = document.querySelector("#search-form-button");
-  let bodyBackground = document.querySelector("video");
-
-  if (body.classList.contains("dark")) {
-    body.classList.remove("dark");
-    headerElement.classList.remove("dark");
-    footerElement.classList.remove("dark");
-    searchElement.classList.remove("dark");
-    searchButton.classList.remove("dark");
-    bodyBackground.src = "videos/clouds.mp4";
-  } else {
-    body.classList.add("dark");
-    headerElement.classList.add("dark");
-    footerElement.classList.add("dark");
-    searchElement.classList.add("dark");
-    searchButton.classList.add("dark");
-    bodyBackground.src = "videos/darkClouds.mp4";
-  }
-}
 
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#currentTemp");
@@ -136,8 +112,5 @@ function displayForecast(response) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-let themeButton = document.querySelector("#themeButton");
-themeButton.addEventListener("click", changeTheme);
 
 searchCity("Paris");
